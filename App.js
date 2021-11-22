fe9ace65bf34d0c97947f42e679a33e6031fbd32
Danushram Vijayakuamr <danushramv12@gmail.com>
2021-11-22 23:50:22 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import './App.css';
+import {Daddy} from './Components/Daddy.js';
+import Quote from './Components/Quote';
+
+
+const App = () =>{
+
+  return (
+    <div className="App">
+      <Daddy />
+      </div>
+  );
+}
+
+export default App;
